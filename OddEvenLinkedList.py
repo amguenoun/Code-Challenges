9f960b9b980c32fb29a22925094834747e0abd14
Ahmad Guenoun <amguenoun@gmail.com>
2020-05-07 22:52:32 -0400

    oddEvenLinkedLIst completed

---
+++ OddEvenLinkedList.py
@@ -0,0 +1,56 @@
+'''
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+
+Note:
+
+    The relative order inside both the even and odd groups should remain as it was in the input.
+    The first node is considered odd, the second node even and so on ...
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        if not head.next or not head.next.next:
+            return head
+
+        lastOdd = head
+        evenHead = head.next
+        lastEven = head.next
+
+        current = head.next.next
+        index = 3
+        while current:
+            if index % 2 == 1:
+                lastOdd.next = current
+                lastOdd = lastOdd.next
+            else:
+                lastEven.next = current
+                lastEven = lastEven.next
+            index += 1
+            current = current.next
+
+        lastEven.next = None
+        lastOdd.next = evenHead
+
+        return head
