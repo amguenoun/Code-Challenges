7e8ff80742bef98221e6a7192d63c565932de1f5
Ahmad Guenoun <amguenoun@gmail.com>
2020-06-11 15:22:20 -0500

    MergeSortedArray

---
+++ MergeSortedArray.py
@@ -0,0 +1,40 @@
+'''
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+    The number of elements initialized in nums1 and nums2 are m and n respectively.
+    You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+'''
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        offset = 0
+        index2 = 0
+
+        if n == 0:
+            return
+        elif n == len(nums1):
+            for i in range(n):
+                nums1[i] = nums2[i]
+            return
+
+        for i in range(m+n):
+            # if i is greater than the last index in nums1 + the offset (added nums from nums2)
+            if i > m+offset-1:
+                nums1[i] = nums2[index2]
+                index2 += 1
+            elif index2 < n and nums1[i] > nums2[index2]:
+                nums1.insert(i, nums2[index2])
+                nums1.pop()
+                offset += 1
+                index2 += 1
