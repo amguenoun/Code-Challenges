b958dfd2bdf320b17317ac7a427b53b7bfe41398
Ahmad Guenoun <amguenoun@gmail.com>
2020-08-12 10:45:18 -0500

    UglyNumber

---
+++ UglyNumber.py
@@ -0,0 +1,42 @@
+'''
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+
+Example 3:
+
+Input: 14
+Output: false 
+Explanation: 14 is not ugly since it includes another prime factor 7.
+'''
+
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+
+        if num == 0:
+            return False
+
+        given_prime = [2, 3, 5]
+
+        for p in given_prime:
+
+            while num % p == 0:
+                num //= p
+
+        if num == 1:
+            return True
+        else:
+            return False
